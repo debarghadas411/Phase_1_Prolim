519826e9e4ed68c13ae02708d10cd71237bca69c
debar <debar@LAPTOP-02171SP5.mshome.net>
2021-06-02 18:02:39 +0530

    2nd DAY

---
+++ src/prolim_phase1_core/HashMapExample.java
@@ -0,0 +1,17 @@
+package prolim_phase1_core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapExample {
+	public static void main(String[] args) {
+		HashMap<Integer,String> hm = new HashMap<Integer,String>();
+		hm.put(1,"Hello");
+		hm.put(2, "World");
+		hm.put(3, "Java");
+		for(Map.Entry m:hm.entrySet()) {
+			System.out.println(m.getKey()+":"+m.getValue());
+		}
+	}
+
+}
--- src/prolim_phase1_core/LinkedHashMapExample.java
+++
@@ -1,8 +0,0 @@
-package prolim_phase1_core;
-
-public class LinkedHashMapExample {
-	public static void main(String[] args) {
-		
-	}
-
-}
